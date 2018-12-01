d9f7e02e3dfe124857f9e0b46bf092c2021633c0
simplyK8 <nova.kplat@gmail.com>
2018-12-01 03:24:08 +0300

    eva characters cards done

--- src/clickCounter/script.js
+++ src/clickCounter/script.js
@@ -1,56 +1,56 @@
-const clickBtn = document.getElementById('click-btn')
-const clickValue = document.getElementById('click-value')
-const clickReset = document.getElementById('click-reset')
-const increaseStep = document.getElementById('increase-step')
-const stepValue = document.getElementById('step-value')
-const increaseValue = document.getElementById('increase-value')
-const valueIncreased = document.getElementById('value-increased')
-const clicksLeft = document.getElementById('clicks-left')
+// const clickBtn = document.getElementById('click-btn')
+// const clickValue = document.getElementById('click-value')
+// const clickReset = document.getElementById('click-reset')
+// const increaseStep = document.getElementById('increase-step')
+// const stepValue = document.getElementById('step-value')
+// const increaseValue = document.getElementById('increase-value')
+// const valueIncreased = document.getElementById('value-increased')
+// const clicksLeft = document.getElementById('clicks-left')
 
-const MAX_VALUE = 20
-const CLICKS_VALUE = 0
-const STEP = 1
-const VALUE_INCREASE = 10
+// const MAX_VALUE = 20
+// const CLICKS_VALUE = 0
+// const STEP = 1
+// const VALUE_INCREASE = 10
 
-let clicks = CLICKS_VALUE
-let step = STEP
-let maxValue = MAX_VALUE
+// let clicks = CLICKS_VALUE
+// let step = STEP
+// let maxValue = MAX_VALUE
 
-clickBtn.onclick = function() {
-	if (clicks + step <= maxValue) {
-		clicks = clicks + step
+// clickBtn.onclick = function() {
+// 	if (clicks + step <= maxValue) {
+// 		clicks = clicks + step
 
-		clicksLeft.innerHTML = maxValue - clicks
-		clickValue.innerHTML = clicks
-	} else {
-		clickValue.innerHTML = 'max value reached'
-		clickBtn.disabled = true
-		clicksLeft.innerHTML = CLICKS_VALUE
-	}
-}
+// 		clicksLeft.innerHTML = maxValue - clicks
+// 		clickValue.innerHTML = clicks
+// 	} else {
+// 		clickValue.innerHTML = 'max value reached'
+// 		clickBtn.disabled = true
+// 		clicksLeft.innerHTML = CLICKS_VALUE
+// 	}
+// }
 
-clickReset.onclick = function() {
-	clicks = CLICKS_VALUE
-	maxValue = MAX_VALUE
-	step = STEP
+// clickReset.onclick = function() {
+// 	clicks = CLICKS_VALUE
+// 	maxValue = MAX_VALUE
+// 	step = STEP
 
-	clickValue.innerHTML = clicks
-	stepValue.innerHTML = step
-	valueIncreased.innerHTML = maxValue
-	clicksLeft.innerHTML = maxValue
-	clickBtn.disabled = false
-}
-increaseStep.onclick = function() {
-	step = step + STEP
+// 	clickValue.innerHTML = clicks
+// 	stepValue.innerHTML = step
+// 	valueIncreased.innerHTML = maxValue
+// 	clicksLeft.innerHTML = maxValue
+// 	clickBtn.disabled = false
+// }
+// increaseStep.onclick = function() {
+// 	step = step + STEP
 
-	clicksLeft.innerHTML = maxValue - clicks
-	stepValue.innerHTML = step
-}
+// 	clicksLeft.innerHTML = maxValue - clicks
+// 	stepValue.innerHTML = step
+// }
 
-increaseValue.onclick = function() {
-	maxValue = maxValue + VALUE_INCREASE
-	valueIncreased.innerHTML = maxValue
-	clicksLeft.innerHTML = maxValue - clicks
-	clickValue.innerHTML = clicks
-	clickBtn.disabled = false
-}
+// increaseValue.onclick = function() {
+// 	maxValue = maxValue + VALUE_INCREASE
+// 	valueIncreased.innerHTML = maxValue
+// 	clicksLeft.innerHTML = maxValue - clicks
+// 	clickValue.innerHTML = clicks
+// 	clickBtn.disabled = false
+// }
---
+++ src/main/script.js
@@ -0,0 +1,138 @@
+const HUMAN = 'human'
+const ROBOT = 'robot'
+const EVAPILOT = 'eva pilot'
+const NERVWORKER = 'nerv worker'
+const EVA = 'eva'
+
+const cards = [
+	{
+		name: 'Eva 01',
+		avatar: 'https://i.pinimg.com/originals/25/9b/fd/259bfddcb90ee8a265392bfbcdfca2c1.png',
+		species: ROBOT,
+		age: 'age unknown',
+		occupation: EVA,
+		badge: 'https://ih0.redbubble.net/image.43104463.5427/flat,1000x1000,075,f.jpg',
+	},
+	{
+		name: 'Eva 02',
+		avatar: 'https://ru.myanimeshelf.com/upload/dynamic/2011-08/31/eva-022.JPG',
+		species: ROBOT,
+		age: 'age unknown',
+		occupation: EVA,
+		badge: 'https://ih0.redbubble.net/image.43104463.5427/flat,1000x1000,075,f.jpg',
+	},
+	{
+		name: 'Eva 00',
+		avatar: 'http://images2.fanpop.com/image/photos/9200000/EVA-00-evangelion-9207743-640-480.jpg',
+		species: ROBOT,
+		age: 'age unknown',
+		occupation: EVA,
+		badge: 'https://ih0.redbubble.net/image.43104463.5427/flat,1000x1000,075,f.jpg',
+	},
+	{
+		name: 'Shinji Ikari',
+		avatar: 'https://pm1.narvii.com/6057/9dae51eb11235139050eae65fdc63205f8264fdb_hq.jpg',
+		species: HUMAN,
+		age: '16',
+		occupation: EVAPILOT,
+		badge: 'https://ih0.redbubble.net/image.43104463.5427/flat,1000x1000,075,f.jpg',
+	},
+
+	{
+		name: 'Asuka Soryu',
+		avatar: 'https://moe.shikimori.org/system/user_images/original/31022/187747.jpg',
+		species: HUMAN,
+		age: '16',
+		occupation: EVAPILOT,
+		badge: 'https://ih0.redbubble.net/image.43104463.5427/flat,1000x1000,075,f.jpg',
+	},
+
+	{
+		name: 'Rei Ayanami',
+		avatar: 'https://pm1.narvii.com/6204/566a1ee2bab152142f2f28418117df005aa6cf94_hq.jpg',
+		species: HUMAN,
+		age: '16',
+		occupation: EVAPILOT,
+		badge: 'https://ih0.redbubble.net/image.43104463.5427/flat,1000x1000,075,f.jpg',
+	},
+
+	{
+		name: 'Misato Katsuragi',
+		avatar: 'https://myanimelist.cdn-dena.com/images/characters/3/317294.jpg',
+		species: HUMAN,
+		age: '30',
+		occupation: NERVWORKER,
+		badge: 'https://ih0.redbubble.net/image.43104463.5427/flat,1000x1000,075,f.jpg',
+	},
+
+	{
+		name: 'Gendo Ikari',
+		avatar: 'https://i.kym-cdn.com/photos/images/newsfeed/000/015/575/Gendo_Ikari_by_Darthval.jpg',
+		species: HUMAN,
+		age: '45',
+		occupation: NERVWORKER,
+		badge: 'https://ih0.redbubble.net/image.43104463.5427/flat,1000x1000,075,f.jpg',
+	},
+
+	{
+		name: 'Ritsuko Akagi',
+		avatar: 'http://animeperson.com/images/character/c11d65125b.jpg',
+		species: HUMAN,
+		age: '32',
+		occupation: NERVWORKER,
+		badge: 'https://ih0.redbubble.net/image.43104463.5427/flat,1000x1000,075,f.jpg',
+	},
+]
+const container = document.getElementById('container')
+
+function canHandleEva(item) {
+	return item.species === HUMAN && item.age < 20
+}
+
+cards.forEach(function(item) {
+	const card = document.createElement('div')
+	card.classList.add('card', 'drop-shadow', 'standard-border', 'text-center', 'm-b', 'card-hover')
+
+	const avatar = document.createElement('img')
+	avatar.src = item.avatar
+	avatar.classList.add('avatar')
+
+	const badge = document.createElement('img')
+	badge.src = item.badge
+	badge.classList.add('badge')
+	badge.style.visibility = 'hidden'
+
+	const name = document.createElement('h2')
+	name.innerHTML = item.name
+	name.classList.add('text-center', 'text-title')
+
+	const species = document.createElement('h3')
+	species.innerHTML = item.species
+	species.classList.add('text-center', 'card-text')
+
+	const age = document.createElement('h3')
+	age.innerHTML = item.age
+	age.classList.add('text-center', 'card-text')
+
+	const occupation = document.createElement('h3')
+	occupation.innerHTML = item.occupation
+	occupation.classList.add('text-center', 'card-text')
+
+	if (item.occupation === EVAPILOT) {
+		name.classList.add('text-accent')
+		badge.style.visibility = 'visible'
+	}
+	if (item.occupation === EVA) {
+		name.classList.add('text-eva')
+	}
+
+	container.append(card)
+	card.append(avatar)
+	card.append(badge)
+	card.append(name)
+	card.append(species)
+	card.append(age)
+	card.append(occupation)
+
+	console.log(canHandleEva(item))
+})
